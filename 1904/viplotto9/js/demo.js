63153206d9e6e508ab4471b424a63296097e570f
mufasagold <hyung0715@gmail.com>
2019-04-12 15:59:44 +0900

    vip로또 바로가기용

---
+++ 1904/viplotto9/js/demo.js
@@ -0,0 +1,53 @@
+$(function() {
+
+if (!$('.demo').length) return;
+
+
+var flipsnap = Flipsnap('#demo-transitionDuration .flipsnap', {
+		distance: 25, 	// 100 나누기 아이템 
+		transitionDuration: 500
+});
+
+//자동  
+/*setInterval(function() {
+	flipsnap.toNext()
+}, 4000); 
+*/ 
+var $next = $('#prev').click(function() {
+	   flipsnap.toNext();
+});
+
+var $next = $('#next').click(function() {
+	   flipsnap.toPrev();
+});
+
+
+var $flipsnapclick = $('.flipsnap').click(function() {
+	   flipsnap.toNext();
+});
+
+/*
+(function transitionDuration() {
+	Flipsnap('#demo-transitionDuration .flipsnap', {
+		distance: 15.8, 
+		transitionDuration: 500
+	});
+})();
+
+
+var $next = $('#next').click(function() {
+});
+
+
+
+
+$('.sample a').click(function(e) {
+  e.preventDefault();
+  var $a = $(this);
+  var $code = $a.parents('.sample').find('pre');
+  $code.slideToggle('fast', function() {
+    $a.text($code.is(':visible') ? 'hide code' : 'show code');
+  });
+});
+*/
+});
